5716cb9779aeee4e6caee84c1d51d64cebd75bfe
inventwithdean <inventwithdean@gmail.com>
2026-09-02 14:42:56 +0530

    add indirect chains b/w npcs

--- Assets/Scripts/WebMCPManager.cs
+++ Assets/Scripts/WebMCPManager.cs
@@ -77,25 +77,68 @@ public class WebMCPManager : MonoBehaviour
         }
 
         string storyConstraints = "";
+        string knowledgeConstraints = "";
+
         // This will always be true, but just to be safe
-        if (availableNPCs.Count >= 4)
+        if (availableNPCs.Count >= 7)
         {
             string killer = availableNPCs[0].npcName;
             string key1 = availableNPCs[1].npcName;
             string key2 = availableNPCs[2].npcName;
             string key3 = availableNPCs[3].npcName;
 
+            string bridge1 = availableNPCs[4].npcName;
+            string bridge2 = availableNPCs[5].npcName;
+            string bridge3 = availableNPCs[6].npcName;
+
             storyConstraints = $"STORY DATA:\n" +
                                $"The Killer is: {killer}\n" +
                                $"The player MUST interact with these 3 key NPCs before catching the killer: {key1}, {key2}, and {key3}.\n" +
                                $"Do not allow the player to successfuly identify or arrest the killer until they have gathered clues from all three of these key NPCs.";
-        }
-        else
-        {
-            Debug.LogWarning("Not enough NPCs in the scene to assign a killer and 3 key NPCs.");
+
+            Dictionary<string, List<string>> npcKnowledge = new Dictionary<string, List<string>>();
+            foreach (var npc in availableNPCs)
+            {
+                npcKnowledge[npc.npcName] = new List<string>();
+            }
+
+            // Establish the indirect breadcrumb chain via random bridges:
+            npcKnowledge[key1].Add(bridge1);
+            npcKnowledge[bridge1].Add(key2);
+
+            npcKnowledge[key2].Add(bridge2);
+            npcKnowledge[bridge2].Add(key3);
+
+            npcKnowledge[key3].Add(bridge3);
+            npcKnowledge[bridge3].Add(killer);
+
+            // Fill the rest of everyone's knowledge slots up to a max of 3 random connections
+            foreach (var npc in availableNPCs)
+            {
+                while (npcKnowledge[npc.npcName].Count < 3)
+                {
+                    string randomName = availableNPCs[Random.Range(0, availableNPCs.Count)].npcName;
+                    if (randomName != npc.npcName && !npcKnowledge[npc.npcName].Contains(randomName))
+                    {
+                        npcKnowledge[npc.npcName].Add(randomName);
+                    }
+                }
+            }
+
+            knowledgeConstraints = "NPC Knowledge Limitations:\n" +
+                                  "NPCs ONLY know the specific people listed below. If the player asks an NPC about someone NOT in their known list, the NPC must deny knowing them or claim ignorance.\n";
+            foreach (var kvp in npcKnowledge)
+            {
+                knowledgeConstraints += $"{kvp.Key} knows: {string.Join(", ", kvp.Value)}\n";
+            }
         }
 
-        ReturnSystemContext($"{systemPrompt}\n\n{storyConstraints}\n\n{rosterContext}\n\n{locationsContext}");
+        else
+        {
+            Debug.LogWarning("Not enough NPCs in the scene to assign a killer and 3 key NPCs or to setup indirect knowledge graph. Make sure the scene has atleast 7 NPCs.");
+        }
+
+        ReturnSystemContext($"{systemPrompt}\n\n{storyConstraints}\n\n{knowledgeConstraints}\n\n{rosterContext}\n\n{locationsContext}");
     }
 
 }
